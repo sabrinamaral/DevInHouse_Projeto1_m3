dd0238cb0ba1121c2e74a186bd02b36ff223c5c7
sabrinamaral <sabrinamaral@hotmail.com>
2022-04-21 18:14:48 -0300

    sentry added

--- src/server.js
+++ src/server.js
@@ -8,11 +8,26 @@ const swaggerFile = require("./swagger.json");
 const logger = require("./config/logger");
 const morgan = require("morgan");
 const morganLog = require("./config/morgan");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 
 app.use(express.json());
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use(morgan("combined", { stream: morganLog }));
-
 app.use(routes);
+app.use(Sentry.Handlers.errorHandler());
+
+Sentry.init({
+  dsn: process.env.SENTRY,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 logger.info("aplicação online");
