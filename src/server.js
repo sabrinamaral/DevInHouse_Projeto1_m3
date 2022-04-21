b6b28661cab53374453bb81a4c5bcd599bfcd9a2
sabrinamaral <sabrinamaral@hotmail.com>
2022-04-21 15:11:30 -0300

    Morgan added

--- src/server.js
+++ src/server.js
@@ -5,11 +5,16 @@ const routes = require("./routes");
 require("./database");
 const swaggerUI = require("swagger-ui-express");
 const swaggerFile = require("./swagger.json");
-const Logger = require("./config/logger");
+const logger = require("./config/logger");
+const morgan = require("morgan");
+const morganLog = require("./config/morgan");
 
 app.use(express.json());
+app.use(morgan("combined", { stream: morganLog }));
+
 app.use(routes);
+
 app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
-Logger.info("aplicação online");
+logger.info("aplicação online");
 
 app.listen(PORT, () => console.log(`Executando na porta ${PORT}`));
