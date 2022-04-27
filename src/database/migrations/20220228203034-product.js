a109b5c8ea711d32b76ecb6232ed6d6764517d14
sabrinamaral <sabrinamaral@hotmail.com>
2022-04-26 23:19:29 -0300

    adjustment in product migration

--- src/database/migrations/20220228203034-product.js
+++ src/database/migrations/20220228203034-product.js
@@ -11,6 +11,7 @@ module.exports = {
       },
       category_id: {
         type: Sequelize.INTEGER,
+        allowNull: false,
         references: {
           model: {
             tableName: "products",
